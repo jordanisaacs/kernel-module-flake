c7af5ea003172ddd476ec5cbbe9d5b6ee03fe38b
Jordan Isaacs <mail@jdisaacs.com>
2023-01-05 10:41:29 +0700

    add search paths

--- build/run-gdb.nix
+++ build/run-gdb.nix
@@ -8,18 +8,24 @@
   modules ? [],
 }: let
   moduleDirs =
+    builtins.map
+    (m: "${m}/lib/modules/${kernel.modDirVersion}/misc")
+    modules;
+
+  symbolsDirs =
     builtins.concatStringsSep
     " "
-    (builtins.map
-      (m: "${m}/lib/modules/${kernel.modDirVersion}/misc")
-      modules);
+    moduleDirs;
+
+  searchDirs = builtins.concatStringsSep " " (builtins.map (m: ''-ex "dir ${m}"'') moduleDirs);
 in
   # Need a to go a directroy above source (eg. scripts) because vmlinux is using relative path
   writeScriptBin "rungdb" ''
     ${gdb}/bin/gdb \
+      ${searchDirs} \
       -ex "dir ${kernel.dev}/lib/modules/${kernel.modDirVersion}/source/scripts" \
       -ex "file ${kernel.dev}/vmlinux" \
       -ex "source ${kernel.dev}/lib/modules/${kernel.modDirVersion}/build/vmlinux-gdb.py" \
-      -ex "alias lx-symbols-nix = lx-symbols ${moduleDirs}" \
+      -ex "alias lx-symbols-nix = lx-symbols ${symbolsDirs}" \
       -ex "target remote localhost:1234"
   ''
